c4f2dd7cd78a5c87331bf1c355bd219cf36fd9e5
GreenNinjaRabbit <green.ninja.rabbit@gmail.com>
2024-01-01 19:26:30 +0100

    on when held

--- Guide/password/password.py
+++ Guide/password/password.py
@@ -1,12 +1,20 @@
+import signal
 from gpiozero import LED, Button
 
 redled = LED(pin=17)
-small_button = Button(pin=18)
+small_button = Button(pin=18, hold_time=2)
 
 redled.blink(on_time=3, off_time=2, n=3, background=False)
 
-while True:
-    if small_button.is_held:
-        redled.on()
-    else:
-        redled.off()
+def ledon():
+    redled.on()
+    print("on")
+
+def ledoff():
+    redled.off()
+    print("off")    
+
+small_button.when_held = ledon
+small_button.when_deactivated = ledoff
+
+signal.pause()

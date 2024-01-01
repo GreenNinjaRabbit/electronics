b5faeb4254d993db392fde01e0c4691343c77cb3
GreenNinjaRabbit <green.ninja.rabbit@gmail.com>
2024-01-01 17:14:50 +0100

    hold to light

--- Guide/password/password.py
+++ Guide/password/password.py
@@ -1,4 +1,12 @@
-from gpiozero import LED
+from gpiozero import LED, Button
 
 redled = LED(pin=17)
+small_button = Button(pin=18)
+
 redled.blink(on_time=3, off_time=2, n=3, background=False)
+
+while True:
+    if small_button.is_held:
+        redled.on()
+    else:
+        redled.off()

7b1c230e5a1e8ac5f440e40db897f270830dd408
GreenNinjaRabbit <green.ninja.rabbit@gmail.com>
2024-01-03 18:36:06 +0100

    test other keys works

--- password/test_keypad.py
+++ password/test_keypad.py
@@ -1,8 +1,9 @@
 from keypad import Keypad
 
+inputpins = [12,16,18,22]
+outputpins = [11,13,15,19]
+
 def test_Keypad():
-    inputpins = [12,16,18,22]
-    outputpins = [11,13,15,19]
     keys = [
         ["1","2","3","A"],
         ["4","5","6","B"],
@@ -10,6 +11,18 @@ def test_Keypad():
         ["*","0","#","D"]
     ]
     pad = Keypad(inputpins=inputpins, outputpins=outputpins)
+    assert pad.inputpins == inputpins
+    assert pad.outputpins == outputpins
+    assert pad.keys == keys
+
+def test_otherkeys():
+    keys = [
+        ["1","2","3"],
+        ["4","5","6"],
+        ["7","8","9"],
+        ["*","0","#"]
+    ]
+    pad = Keypad(inputpins=inputpins, outputpins=outputpins, keys = keys)
     assert pad.inputpins == inputpins
     assert pad.outputpins == outputpins
     assert pad.keys == keys

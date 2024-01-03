87cb28d811775f91a7a8524022225676bbadd1e8
GreenNinjaRabbit <green.ninja.rabbit@gmail.com>
2024-01-03 17:36:13 +0100

    keypad knows its keys

--- password/keypad.py
+++ password/keypad.py
@@ -1,4 +1,11 @@
 class Keypad:
-    def __init__(self, inputpins, outputpins) -> None:
+    KEYS = [
+        ["1","2","3","A"],
+        ["4","5","6","B"],
+        ["7","8","9","C"],
+        ["*","0","#","D"]
+    ]
+    def __init__(self, inputpins, outputpins, keys=KEYS) -> None:
         self.inputpins = inputpins
         self.outputpins = outputpins
+        self.keys = keys
--- password/test_keypad.py
+++ password/test_keypad.py
@@ -3,6 +3,13 @@ from keypad import Keypad
 def test_Keypad():
     inputpins = [12,16,18,22]
     outputpins = [11,13,15,19]
+    keys = [
+        ["1","2","3","A"],
+        ["4","5","6","B"],
+        ["7","8","9","C"],
+        ["*","0","#","D"]
+    ]
     pad = Keypad(inputpins=inputpins, outputpins=outputpins)
     assert pad.inputpins == inputpins
     assert pad.outputpins == outputpins
+    assert pad.keys == keys

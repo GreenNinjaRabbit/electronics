b2f6489a5101789afb9c654cfe6ac6af42876c64
GreenNinjaRabbit <green.ninja.rabbit@gmail.com>
2024-01-06 20:13:38 +0100

    get pressed key from active pins

--- password/keypad.py
+++ password/keypad.py
@@ -1,3 +1,5 @@
+from gpiozero import Device
+
 class Keypad:
     KEYS = [
         ["1","2","3","A"],
@@ -6,7 +8,16 @@ class Keypad:
         ["*","0","#","D"]
     ]
     def __init__(self, inputpins, outputpins, keys=KEYS) -> None:
-        self.inputpins = inputpins
-        self.outputpins = outputpins
+        self.inputpinnumbers = inputpins
+        self.inputpins = [Device.pin_factory.pin(pinnumber) for pinnumber in inputpins]
+        self.outputpinnumbers = outputpins
+        self.outputpins = [Device.pin_factory.pin(pinnumber) for pinnumber in outputpins]
         self.keys = keys
-        self.value = "5"
+        
+    @property
+    def value(self):
+        for row, inputpin in enumerate(self.inputpins):
+            if inputpin.state:
+                for col, outputpin in enumerate(self.outputpins):
+                    if outputpin.state:
+                        return self.keys[row][col]
--- password/test_keypad.py
+++ password/test_keypad.py
@@ -5,7 +5,20 @@ from gpiozero.pins.mock import MockConnectedPin, MockPin
 inputpins = [12,16,18,22]
 outputpins = [11,13,15,19]
 
-def test_Keypad():
+class MockPressedButton(MockPin):
+    """
+    This derivative of :class:`MockPin` emulates a pin connected to another
+    mock pin e.g. by pressing a button.
+    """
+    def __init__(self, factory, info, connected_pin):
+        super().__init__(factory, info)
+        self.connected_pin = connected_pin
+    
+    def _change_state(self, value):
+        self.connected_pin.state = value
+        return super()._change_state(value)
+
+def test_Keypad(mock_factory):
     keys = [
         ["1","2","3","A"],
         ["4","5","6","B"],
@@ -13,11 +26,11 @@ def test_Keypad():
         ["*","0","#","D"]
     ]
     pad = Keypad(inputpins=inputpins, outputpins=outputpins)
-    assert pad.inputpins == inputpins
-    assert pad.outputpins == outputpins
+    assert pad.inputpinnumbers == inputpins
+    assert pad.outputpinnumbers == outputpins
     assert pad.keys == keys
 
-def test_otherkeys():
+def test_otherkeys(mock_factory):
     keys = [
         ["1","2","3"],
         ["4","5","6"],
@@ -25,25 +38,11 @@ def test_otherkeys():
         ["*","0","#"]
     ]
     pad = Keypad(inputpins=inputpins, outputpins=outputpins, keys = keys)
-    assert pad.inputpins == inputpins
-    assert pad.outputpins == outputpins
+    assert pad.inputpinnumbers == inputpins
+    assert pad.outputpinnumbers == outputpins
     assert pad.keys == keys
 
 def test_5pressed(mock_factory):
-    class MockPressedButton(MockPin):
-        """
-        This derivative of :class:`MockPin` emulates a pin connected to another
-        mock pin e.g. by pressing a button.
-        """
-        def __init__(self, factory, info, connected_pin):
-            super().__init__(factory, info)
-            self.connected_pin = connected_pin
-        
-        def _change_state(self, value):
-            self.connected_pin.state = value
-            return super()._change_state(value)
-
-
     pin13 = mock_factory.pin(13)
     pin16 = mock_factory.pin(16, pin_class=MockPressedButton, connected_pin=pin13)
     pin13.function = "output"
@@ -56,3 +55,17 @@ def test_5pressed(mock_factory):
     assert pin13.state is True
     pad = Keypad(inputpins=inputpins, outputpins=outputpins)
     assert pad.value == "5"
+
+def test_9pressed(mock_factory):
+    pin18 = mock_factory.pin(18)
+    pin15 = mock_factory.pin(15, pin_class=MockPressedButton, connected_pin=pin18)
+    pin18.function = "output"
+    pin15.function = "input"
+    assert pin15.state is False
+    assert pin18.state is False
+    pin15.drive_high()
+    sleep(1)
+    assert pin15.state is True
+    assert pin18.state is True
+    pad = Keypad(inputpins=inputpins, outputpins=outputpins)
+    assert pad.value == "9"
